16799b4dae1932ada767f4c72f11cfb6b1c95edb
Pamela <pamelamarinhopm@hotmail.com>
2021-08-02 11:20:16 -0300

    verbo post

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -4,8 +4,13 @@ const router = express.Router();
 
 //pode usar somente '/' se no app **
 router.get('/checklist', (req, res) => {
-    console.log('aqui tem o checklist')
+    console.log('foi feita uma requisição get')
     res.send('rota certa');
 })
 
+router.post('/checklist', (req, res) => {
+    console.log('foi feita uma requisição tipo post')
+    console.log(req.body)
+    res.status(200).json(req.body)
+})
 module.exports = router;
