1116dc652ad62aaee84947bb30aea186fb2859bb
Pamela <pamelamarinhopm@hotmail.com>
2021-08-03 02:47:46 -0300

    mongodb/mongoose/conectando com o node

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const checkListRouter = require('./src/routes/checklist.js')
 const app = express()
+require('./config/database') //importando configuração. quando der o require o database.js vai rodar e conectar com o bd
 
 /*use: comando para usar middleware. Vai ver se tem arquivo json e se tiver
 vai deixar disponível no req.body(body da requisição*/
