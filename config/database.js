78a17114c9053df12255f2ed673e1ac74f3397a4
Pamela <pamelamarinhopm@hotmail.com>
2021-08-03 03:11:31 -0300

    conectando mongoose com o mongodb

--- config/database.js
+++ config/database.js
@@ -5,3 +5,6 @@ mongoose.Promise = global.Promise
 mongoose.connect('mongodb://localhost/todo-list', {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => console.log('conectado ao mongodb'))
     .catch(err=> console.log(err))
+
+    /*useNewUrlParser=> pq mongoose.connect é obsoleto. o drive mongodb reescreveu a ferramenta que analisa string de conexão, para usar o novo analisador de string é necessário usar essa flag. 
+    useUnifiedTopology=> remove suporte para várias opções de conexões que não são mais relevantes com o novo mecanismo de topologia*/
