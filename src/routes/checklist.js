cfa5c11a22ddee9fcf2358a27d1ef5344dc7cc75
Pamela <pamelamarinhopm@hotmail.com>
2021-08-05 01:36:02 -0300

    rotas update, delete e get por id

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -4,9 +4,13 @@ const router = express.Router()
 const Checklist = require('../models/checklist')
 
 // pode usar somente '/' se no app **
-router.get('/', (req, res) => {
-    console.log('foi feita uma requisição get')
-    res.send('rota certa')
+router.get('/', async (req, res) => {
+    try {
+        let checklist = await Checklist.find({})
+        res.status(200).json(checklist)
+    } catch (error) {
+        res.status(422).json(error)
+    }
 })
 
 router.post('/', async (req, res) => {
@@ -19,19 +23,34 @@ router.post('/', async (req, res) => {
     }
 })
 
-router.get('/:id', (req, res) => {
-    console.log(req.params.id)
-    res.send(`ID ${req.params.id}`)
+router.get('/:id', async (req, res) => {
+    let id = req.params.id
+    try {
+        let checklist = await Checklist.findById(id)
+        res.status(200).json(checklist)
+    } catch (error) {
+        res.status(422).json(error)
+    }
 })
 
-router.put('/:id', (req, res) => {
-    console.log(req.params.id)
-    res.send(`PUT ID: ${req.params.id}`)
+router.put('/:id', async (req, res) => {
+    let {name} = req.body
+    try {        
+        let checklist = await Checklist.findByIdAndUpdate(req.params.id, {name},{new:true})
+        res.status(200).json(checklist)
+    } catch (error) {
+        res.status(422).json(error)
+    }
 })
 
-router.delete('/:id', (req, res) => {
-    console.log (req.params.id)
-    res.send(`DELETE ID: ${req.params.id}`)
+router.delete('/:id', async (req, res) => {
+    let id = req.params.id
+    try {
+      let checklist =  await Checklist.findByIdAndDelete(id)
+      res.status(200).json(checklist)
+    } catch (error) {
+        res.status(422).json(error)
+    }
 })
 
 module.exports = router;
